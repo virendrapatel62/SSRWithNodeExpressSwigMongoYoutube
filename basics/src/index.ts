ff6b51dc59242316790184b7326db3c350f9853a
Virendra patel <patelvirendra62@gmail.com>
2024-10-23 23:21:02 +0530

    Formated code

--- basics/src/index.ts
+++ basics/src/index.ts
@@ -1,42 +1,50 @@
-import express from 'express';
-import { homePageController } from './controllers/home';
-import { getAllUsersController, getUserByIdController } from './controllers/user';
-import swig from 'swig'
-import minifier from 'html-minifier'
+import express from "express";
+import { homePageController } from "./controllers/home";
+import {
+  getAllUsersController,
+  getUserByIdController,
+} from "./controllers/user";
+import swig from "swig";
+import minifier from "html-minifier";
 
 const app = express();
 
 swig.setDefaults({
-  cache: false
+  cache: false,
 });
 
 app.set("view engine", "html");
 // app.engine("html", require("swig").renderFile);
 
-const minifyHTML = false
+const minifyHTML = false;
 
 app.engine("html", (path, object, cb) => {
-  return require("swig").renderFile(path, object, (error: Error, html: string) => {
-    if (error) {
-      return cb(error, html)
-    }
+  return require("swig").renderFile(
+    path,
+    object,
+    (error: Error, html: string) => {
+      if (error) {
+        return cb(error, html);
+      }
 
-    if (minifyHTML) {
-      html = minifier.minify(html, {
-        collapseWhitespace: true, removeTagWhitespace: true,
-        removeComments: true, minifyCSS: true
-      })
-    }
+      if (minifyHTML) {
+        html = minifier.minify(html, {
+          collapseWhitespace: true,
+          removeTagWhitespace: true,
+          removeComments: true,
+          minifyCSS: true,
+        });
+      }
 
-    cb(error, html)
-  })
+      cb(error, html);
+    }
+  );
 });
 
 app.listen(3000, () => {
   console.log("Listening on port 300");
   console.log("Visit http://localhost:3000");
 });
-
 
 app.get("/", homePageController);
 app.get("/users", getAllUsersController);
