f9713f812656551818c45b1050f6e8f9f8575f83
Virendra patel <patelvirendra62@gmail.com>
2025-01-05 14:16:51 +0530

    Updated env

--- basics/build/src/index.js
+++ basics/build/src/index.js
@@ -3,16 +3,24 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+require("./db");
 const express_1 = __importDefault(require("express"));
 const home_1 = require("./controllers/home");
 const user_1 = require("./controllers/user");
 const swig_1 = __importDefault(require("swig"));
 const html_minifier_1 = __importDefault(require("html-minifier"));
 const path_1 = __importDefault(require("path"));
+const body_parser_1 = __importDefault(require("body-parser"));
 const app = (0, express_1.default)();
 app.use(express_1.default.static(path_1.default.join(__dirname, "../public")));
+app.use(body_parser_1.default.urlencoded({
+    extended: true,
+}));
 swig_1.default.setDefaults({
     cache: false,
+    loader: swig_1.default.loaders.fs(path_1.default.join(__dirname, "../views")),
 });
 app.set("view engine", "html");
 // app.engine("html", require("swig").renderFile);
@@ -33,10 +41,12 @@ app.engine("html", (path, object, cb) => {
         cb(error, html);
     });
 });
-app.listen(3000, () => {
-    console.log("Listening on port 300");
-    console.log("Visit http://localhost:3000");
+app.listen(process.env.PORT, () => {
+    console.log("Listening on port ", process.env.PORT);
+    console.log(`Visit http://localhost:${process.env.PORT}`);
 });
 app.get("/", home_1.homePageController);
 app.get("/users", user_1.getAllUsersController);
+app.get("/users/add", user_1.addUserPageController);
+app.post("/users/add", user_1.addUserController);
 app.get("/users/:userId", user_1.getUserByIdController);
--- basics/src/db/index.ts
+++ basics/src/db/index.ts
@@ -1,12 +1,5 @@
 const mongoose = require("mongoose");
-
-const dbName = process.env.MONGO_DB_DATABASE_NAME;
-const username = process.env.MONGO_DB_USER_NAME;
-const password = process.env.MONGO_DB_PASSWORD;
-
-console.log({ dbName });
-
-const url = `mongodb+srv://${username}:${password}@learningclustor01.5qlnc.mongodb.net/${dbName}?retryWrites=true&w=majority&appName=LearningClustor01`;
+const url = process.env.MONGO_DB_CONNECTION_URL;
 mongoose.connect(url).then(() => {
   console.log("Mongo DB Connected..");
 });
